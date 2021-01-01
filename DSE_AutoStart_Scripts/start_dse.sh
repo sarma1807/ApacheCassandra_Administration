a35e0303b957053dd719922df839142daca9305c
Sarma Pydipally <sarma1807@users.noreply.github.com>
2021-01-01 00:52:38 -0500

    Add files via upload

---
+++ DSE_AutoStart_Scripts/start_dse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#########################
+source ~/.bash_profile
+export LOGFILE=~/logs/dse_startup.log
+
+### stop Cassandra
+echo `printf "%0.s-" {1..20}` >> $LOGFILE
+echo `date` >> $LOGFILE
+echo "Attempting to stop DSE (if it is already running) ..." >> $LOGFILE
+dse cassandra-stop
+sleep 30
+
+### start Cassandra
+echo `printf "%0.s-" {1..20}` >> $LOGFILE
+echo `date` >> $LOGFILE
+echo "Attempting to start DSE ..." >> $LOGFILE
+dse cassandra
+sleep 5
+#########################
+exit 0
+
---
+++ DSE_AutoStart_Scripts/stop_dse.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#########################
+source ~/.bash_profile
+export CASSANDRA_LOG_DIR=~/logs
+export LOGFILE=$CASSANDRA_LOG_DIR/dse_shutdown.log
+
+### stop Cassandra
+echo `printf "%0.s-" {1..20}` >> $LOGFILE
+echo `date` >> $LOGFILE
+echo "Attempting to stop DSE (if it is already running) ..." >> $LOGFILE
+dse cassandra-stop
+sleep 30
+#########################
+exit 0
+
