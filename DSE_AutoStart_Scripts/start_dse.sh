b8417d35ac883da02356fb5f5e44c82441af2d35
Sarma Pydipally <sarma1807@users.noreply.github.com>
2023-09-10 17:42:21 -0400

    Add files via upload

--- DSE_AutoStart_Scripts/start_dse.sh
+++ DSE_AutoStart_Scripts/start_dse.sh
@@ -7,6 +7,7 @@ export LOGFILE=~/logs/dse_startup.log
 echo `printf "%0.s-" {1..20}` >> $LOGFILE
 echo `date` >> $LOGFILE
 echo "Attempting to stop DSE (if it is already running) ..." >> $LOGFILE
+nodetool drain
 dse cassandra-stop
 sleep 30
 
--- DSE_AutoStart_Scripts/stop_dse.sh
+++ DSE_AutoStart_Scripts/stop_dse.sh
@@ -8,6 +8,7 @@ export LOGFILE=$CASSANDRA_LOG_DIR/dse_shutdown.log
 echo `printf "%0.s-" {1..20}` >> $LOGFILE
 echo `date` >> $LOGFILE
 echo "Attempting to stop DSE (if it is already running) ..." >> $LOGFILE
+nodetool drain
 dse cassandra-stop
 sleep 30
 #########################
